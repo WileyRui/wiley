2ca96a356af728eb2adf1ec7cd25e1ffa5b603a3
wenrui <15121985208@163.com>
2017-06-03 16:30:31 +0800

    js

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -1,12 +1,27 @@
-$(document).ready(function(){
+$(document).ready(function() {
 	$("#password").change(function() {
 		var pwd = $("#password").val();
 		if (pwd.length < 8) {
 			$("#button").css("background-color", "gray");
-			$("#button").attr("disabled",true);
+			$("#button").attr("disabled", true);
 		} else {
 			$("#button").css("background-color", "#4a77d4");
 			$("#button").removeAttr("disabled");
 		}
+	});
+
+	$("#button").click(function(){
+
+		$.ajax({
+			type : "POST",
+			url : "/paySys/userLogin",
+			data : $('#loginForm').serialize(),
+			success : function(data){
+					alert(data);
+			},
+			error : function(e){
+				alert("error!");
+			}
+		});
 	});
 });
