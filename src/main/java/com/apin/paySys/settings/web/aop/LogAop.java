544cac8501e6e89c18469702ab14e8d7a0981592
wenrui <15121985208@163.com>
2017-07-26 16:15:26 +0800

    完善切面

--- src/main/java/com/apin/paySys/settings/web/aop/LogAop.java
+++ src/main/java/com/apin/paySys/settings/web/aop/LogAop.java
@@ -4,8 +4,6 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -28,15 +26,21 @@ public class LogAop {
 
 	@Before("SysLogPointcut()")
 	private void startTime(JoinPoint point) {
-		// 执行方法名
-		String methodName = point.getSignature().getName();
-		String className = point.getTarget().getClass().getSimpleName();
-		// *========控制台输出=========*//
-		System.out.println("=====通知开始=====");
-		System.out.println(currentTime());
-		System.out.println("请求方法:" + className + "." + methodName + "()");
-		System.out.println(currentTime());
-		System.out.println("=====通知结束=====");
+		try {
+			// 执行方法名
+			String methodName = point.getSignature().getName();
+			String className = point.getTarget().getClass().getSimpleName();
+			// *========控制台输出=========*//
+			System.out.println("=====通知开始=====");
+			System.out.println(currentTime());
+			System.out.println("请求方法:" + className + "." + methodName + "()");
+			System.out.println(currentTime());
+			System.out.println("=====通知结束=====");
+		} catch (Exception e) {
+			// 记录本地异常日志
+			logger.error("==后置通知异常==");
+			logger.error("异常信息:{}", e.getMessage());
+		}
 	}
 
 	/**
--- src/main/java/com/apin/paySys/settings/web/controller/LoginController.java
+++ src/main/java/com/apin/paySys/settings/web/controller/LoginController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.apin.paySys.common.annotation.SysLog;
 import com.apin.paySys.settings.kernel.mapper.SysUserMapper;
 
 /**
@@ -27,86 +28,87 @@ import com.apin.paySys.settings.kernel.mapper.SysUserMapper;
 @RequestMapping("/")
 public class LoginController {
 
-    private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
+	private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
 
-    @Autowired
-    SysUserMapper sysUserMapper;
+	@Autowired
+	SysUserMapper sysUserMapper;
 
-    /**
-     * 进入登录页面
-     * 
-     * @param model
-     * @return
-     */
-    @RequestMapping("login")
-    public String loginUI(Model model) {
-        model.addAttribute("name", "hello");
-        return "login";
-    }
+	/**
+	 * 进入登录页面
+	 * 
+	 * @param model
+	 * @return
+	 */
+	@RequestMapping("login")
+	public String loginUI(Model model) {
+		model.addAttribute("name", "hello");
+		return "login";
+	}
 
-    /**
-     * 用户登录
-     * 
-     * @param userName
-     * @param password
-     * @return
-     */
-    @RequestMapping("userLogin")
-    @ResponseBody
-    public Map<String, Object> login(String userName, String password, Boolean rememberMe, String securityCode,
-            RedirectAttributes redirectAttributes) {
-        Map<String, Object> resultMap = new LinkedHashMap<String, Object>();
-        if (rememberMe == null) {
-            rememberMe = false;
-        }
-        UsernamePasswordToken token = new UsernamePasswordToken(userName, password, rememberMe);
-        // 获取当前的Subject=
-        Subject currentUser = SecurityUtils.getSubject();
-        Session session = SecurityUtils.getSubject().getSession();
-        String code = (String) session.getAttribute("securityCode");
-        if (securityCode == null || securityCode.equals("")) {
-            resultMap.put("status", 500);
-            resultMap.put("message", "验证码不能为空！");
-            return resultMap;
-        }
+	/**
+	 * 用户登录
+	 * 
+	 * @param userName
+	 * @param password
+	 * @return
+	 */
+	@RequestMapping("userLogin")
+	@ResponseBody
+	@SysLog(module = "用户管理", methods = "登录", description = "测试切面")
+	public Map<String, Object> login(String userName, String password, Boolean rememberMe, String securityCode,
+			RedirectAttributes redirectAttributes) {
+		Map<String, Object> resultMap = new LinkedHashMap<String, Object>();
+		if (rememberMe == null) {
+			rememberMe = false;
+		}
+		UsernamePasswordToken token = new UsernamePasswordToken(userName, password, rememberMe);
+		// 获取当前的Subject=
+		Subject currentUser = SecurityUtils.getSubject();
+		Session session = SecurityUtils.getSubject().getSession();
+		String code = (String) session.getAttribute("securityCode");
+		if (securityCode == null || securityCode.equals("")) {
+			resultMap.put("status", 500);
+			resultMap.put("message", "验证码不能为空！");
+			return resultMap;
+		}
 
-        //转化成小写字母
-        securityCode = securityCode.toLowerCase();
-        //还可以读取一次后把验证码清空，这样每次登录都必须获取验证码
-        //session.removeAttribute("_come");
-        if(!code.equals(securityCode)){
-            resultMap.put("status", 500);
-            resultMap.put("message", "验证码错误！");
-            return resultMap;
-        }
-        try {
-            logger.info("对用户[" + userName + "]进行登录验证..验证开始");
-            currentUser.login(token);
-            logger.info("对用户[" + userName + "]进行登录验证..验证通过");
-            resultMap.put("status", 200);
-            resultMap.put("userName", userName);
-        } catch (Exception e) {
-            logger.info("对用户[" + userName + "]进行登录验证..验证未通过,未知账户");
-            redirectAttributes.addFlashAttribute("message", "未知账户");
-            resultMap.put("status", 500);
-            resultMap.put("message", e.getMessage());
-        }
-        return resultMap;
-    }
+		// 转化成小写字母
+		securityCode = securityCode.toLowerCase();
+		// 还可以读取一次后把验证码清空，这样每次登录都必须获取验证码
+		// session.removeAttribute("_come");
+		if (!code.equals(securityCode)) {
+			resultMap.put("status", 500);
+			resultMap.put("message", "验证码错误！");
+			return resultMap;
+		}
+		try {
+			logger.info("对用户[" + userName + "]进行登录验证..验证开始");
+			currentUser.login(token);
+			logger.info("对用户[" + userName + "]进行登录验证..验证通过");
+			resultMap.put("status", 200);
+			resultMap.put("userName", userName);
+		} catch (Exception e) {
+			logger.info("对用户[" + userName + "]进行登录验证..验证未通过,未知账户");
+			redirectAttributes.addFlashAttribute("message", "未知账户");
+			resultMap.put("status", 500);
+			resultMap.put("message", e.getMessage());
+		}
+		return resultMap;
+	}
 
-    @RequestMapping("index")
-    public String indexUI(String userName, Model model) {
-        model.addAttribute("userName", userName);
-        return "index";
-    }
+	@RequestMapping("index")
+	public String indexUI(String userName, Model model) {
+		model.addAttribute("userName", userName);
+		return "index";
+	}
 
-    /**
-     * 用户退出
-     * 
-     * @return
-     */
-    @RequestMapping("logout")
-    public String logout() {
-        return "login";
-    }
+	/**
+	 * 用户退出
+	 * 
+	 * @return
+	 */
+	@RequestMapping("logout")
+	public String logout() {
+		return "login";
+	}
 }
