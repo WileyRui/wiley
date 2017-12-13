2d78a3bc620f8421282e1bd2da9ba6e4e0ae8d83
wenrui <wenrui@apin.com>
2017-12-13 11:41:06 +0800

    修改启动类

--- src/main/java/com/wiley/shiro/Application.java
+++ src/main/java/com/wiley/shiro/Application.java
@@ -1,13 +1,8 @@
 package com.wiley.shiro;
 
-import java.util.HashSet;
-import java.util.Set;
-
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.filter.CharacterEncodingFilter;
@@ -15,22 +10,10 @@ import org.springframework.web.filter.CharacterEncodingFilter;
 @SpringBootApplication
 @EnableTransactionManagement
 @MapperScan("com.wiley.shiro.setting.mapper")//配置mybatis包扫描
-public class Application extends SpringBootServletInitializer {
+public class Application{
 
 	public static void main(String[] args) {
-		SpringApplication app = new SpringApplication(Application.class);
-		app.setWebEnvironment(true);
-
-		Set<Object> set = new HashSet<Object>();
-//		set.add("classpath:config/spring-application.xml");
-//		set.add("classpath:config/spring-mvc.xml");
-		app.setSources(set);
-		app.run(args);
-	}
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(Application.class);
+		SpringApplication.run(Application.class,args);
 	}
 
 	/**
@@ -38,7 +21,6 @@ public class Application extends SpringBootServletInitializer {
 	 * 
 	 * @return
 	 */
-
 	@Bean
 	public CharacterEncodingFilter characterEncodingFilter() {
 		CharacterEncodingFilter filter = new CharacterEncodingFilter();
