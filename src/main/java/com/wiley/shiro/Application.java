44ee4b38bb0558b6c7066301c50a59c02b78590a
wenrui <wenrui@apin.com>
2017-12-28 11:56:21 +0800

    update

--- src/main/java/com/wiley/shiro/Application.java
+++ src/main/java/com/wiley/shiro/Application.java
@@ -2,11 +2,13 @@ package com.wiley.shiro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.filter.CharacterEncodingFilter;
 
+@EnableEurekaClient
 @ComponentScan
 @SpringBootApplication
 @EnableTransactionManagement
